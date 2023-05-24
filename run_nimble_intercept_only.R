55f8029f6137c0eee0bcd2c27efaba6d2f131ba4
robinblythe <robin.blythe@qut.edu.au>
2023-05-24 12:01:06 +1200

    Updated to include single outcome posterior estimation

--- run_nimble_intercept_only.R
+++ run_nimble_intercept_only.R
@@ -4,18 +4,31 @@
 #MCMC iterations, chains, thinning, burn in
 
 run_nimble_intercept_only = function(data, vareq5d, vareqvas, MCMC, n.chains, burnin, thin, seed){
-
+  
   #Define inputs
   utility <- as.numeric(data[[vareq5d]])
   vas <- as.numeric(data[[vareqvas]])/100
+  
   Y <- na.omit(as.matrix(cbind(utility, vas)))
   R <- matrix(data = c(1,0,0,1), nrow = 2)
   N <- nrow(Y)
   
-  #Data - 1 notation is intercept only model
-  bvnorm1 <- list(N = N, Y = Y, R = R)
+  #Data
+  normqs <- list(N = N, Y = utility)
+  normvas <- list(N = N, Y = vas)
+  bvnorm <- list(N = N, Y = Y, R = R)
   
-  eq5dnox <- nimbleCode({
+  eq5d <- nimbleCode({
+    #Model 
+    for (i in 1:N) {
+      Y[i] ~ dnorm(mean = mu[i], tau)
+      mu[i] <- beta
+    }
+    beta ~ dbeta(1,1)
+    tau ~ dgamma(1,1)
+  })
+  
+  eq5dboth <- nimbleCode({
     #Model 
     for (i in 1:N) {
       Y[i,1:2] ~ dmnorm(mean = mu[i,1:2], prec = Omega[1:2,1:2])
@@ -27,22 +40,49 @@ run_nimble_intercept_only = function(data, vareq5d, vareqvas, MCMC, n.chains, bu
     Omega[1:2,1:2] ~ dwish(R[1:2,1:2], 2)
     sigma[1:2, 1:2] <- inverse(Omega[1:2, 1:2])
   })
-
-  constants1 <- list(N = N)
-  inits1 <- list(beta = c(mean(Y[,1]), mean(Y[,2])), Omega = R)
-  inits1 = rep(list(inits1), n.chains) # repeat initial values per chain
   
-  # nimblereg1 <- nimbleMCMC(code = eq5dnox, 
-  #                          data = bvnorm1,
-  #                          inits = inits1, 
-  #                          nchains = n.chains,
-  #                          nburnin = burnin,
-  #                          niter = ifelse(MCMC*n.chains*thin == 0,
-  #                                         MCMC*n.chains + burnin,
-  #                                         MCMC*n.chains*thin + burnin),
-  #                          setSeed = seed,
-  #                          constants = constants1)
-  # browser()
+  constants <- list(N = N)
+  inits1 <- list(beta = mean(utility, na.rm = T), tau = 1)
+  inits1 <- rep(list(inits1), n.chains) # repeat initial values per chain
+  inits2 <- list(beta = c(mean(Y[,1]), mean(Y[,2])), Omega = R)
+  inits2 = rep(list(inits2), n.chains) # repeat initial values per chain
+  
+  # nimblereg1 <- c(
+  #   qs = nimbleMCMC(code = eq5d,
+  #                   data = normqs,
+  #                   inits = inits1, 
+  #                   nchains = n.chains,
+  #                   nburnin = burnin,
+  #                   niter = ifelse(MCMC*n.chains*thin == 0,
+  #                                  MCMC*n.chains + burnin,
+  #                                  MCMC*n.chains*thin + burnin),
+  #                   setSeed = seed,
+  #                   constants = constants),
+  #   
+  #   vas = nimbleMCMC(code = eq5d,
+  #                    data = normvas,
+  #                    inits = inits1,
+  #                    nchains = n.chains,
+  #                    nburnin = burnin,
+  #                    niter = ifelse(MCMC*n.chains*thin == 0,
+  #                                   MCMC*n.chains + burnin,
+  #                                   MCMC*n.chains*thin + burnin),
+  #                    setSeed = seed,
+  #                    constants = constants),
+  #   
+  #   both = nimbleMCMC(code = eq5dboth,
+  #                     data = bvnorm,
+  #                     inits = inits2, 
+  #                     nchains = n.chains,
+  #                     nburnin = burnin,
+  #                     niter = ifelse(MCMC*n.chains*thin == 0,
+  #                                    MCMC*n.chains + burnin,
+  #                                    MCMC*n.chains*thin + burnin),
+  #                     setSeed = seed,
+  #                     constants = constants))
+  
   nimblereg1 <- readRDS("nimble_test_intercept_only.rds")
   nimblereg1
+  
+
 }
--- run_nimble_with_sex.R
+++ run_nimble_with_sex.R
@@ -9,14 +9,28 @@ run_nimble_with_sex = function(data, vareq5d, vareqvas, sex, MCMC, n.chains, bur
   utility <- as.numeric(data[[vareq5d]])
   vas <- as.numeric(data[[vareqvas]])/100
   sex <- as.numeric(data[[sex]])
+  
   Y <- na.omit(as.matrix(cbind(utility, vas)))
   R <- matrix(data = c(1,0,0,1), nrow = 2)
   N <- nrow(Y)
   
-  #Data - 1 notation is intercept only model
-  bvnorm2 <- list(N = N, Y = Y, R = R, sex = sex)
+  #Data
+  normqs <- list(N = N, Y = utility, sex = sex)
+  normvas <- list(N = N, Y = vas, sex = sex)
+  bvnorm <- list(N = N, Y = Y, R = R, sex = sex)
   
-  eq5dwithx <- nimbleCode({
+  eq5d <- nimbleCode({
+    #Model 
+    for (i in 1:N) {
+      Y[i] ~ dnorm(mean = mu[i], tau)
+      mu[i] <- beta[1] + beta[2]*sex[i]
+      }
+    beta[1] ~ dbeta(1,1)
+    beta[2] ~ dnorm(0,10000)
+    tau ~ dgamma(1,1)
+    })
+  
+  eq5dboth <- nimbleCode({
     #Model 
     for (i in 1:N) {
       Y[i,1:2] ~ dmnorm(mean = mu[i,1:2], prec = Omega[1:2,1:2])
@@ -30,21 +44,47 @@ run_nimble_with_sex = function(data, vareq5d, vareqvas, sex, MCMC, n.chains, bur
     sigma[1:2, 1:2] <- inverse(Omega[1:2, 1:2])
   })
   
-  constants2 <- list(N = N)
+  constants <- list(N = N)
+  inits1 <- list(beta = c(mean(utility, na.rm = T), 0), tau = 1)
+  inits1 <- rep(list(inits1), n.chains) # repeat initial values per chain
   inits2 <- list(beta = c(mean(Y[,1]), mean(Y[,2]), 0), Omega = R)
   inits2 = rep(list(inits2), n.chains) # repeat initial values per chain
   
-  nimblereg2 <- nimbleMCMC(code = eq5dwithx, 
-                           data = bvnorm2,
-                           inits = inits2, 
-                           nchains = n.chains,
-                           nburnin = burnin,
-                           niter = ifelse(MCMC*n.chains*thin == 0,
-                                          MCMC*n.chains + burnin,
-                                          MCMC*n.chains*thin + burnin),
-                           setSeed = seed,
-                           constants = constants2)
-  
-  assign("nimblereg2", nimblereg2, envir = .GlobalEnv)
-  
+  # nimblereg2 <- c(
+  #   qs = nimbleMCMC(code = eq5d,
+  #                   data = normqs,
+  #                   inits = inits1, 
+  #                   nchains = n.chains,
+  #                   nburnin = burnin,
+  #                   niter = ifelse(MCMC*n.chains*thin == 0,
+  #                                  MCMC*n.chains + burnin,
+  #                                  MCMC*n.chains*thin + burnin),
+  #                   setSeed = seed,
+  #                   constants = constants),
+  #   
+  #   vas = nimbleMCMC(code = eq5d,
+  #                    data = normvas,
+  #                    inits = inits1,
+  #                    nchains = n.chains,
+  #                    nburnin = burnin,
+  #                    niter = ifelse(MCMC*n.chains*thin == 0,
+  #                                   MCMC*n.chains + burnin,
+  #                                   MCMC*n.chains*thin + burnin),
+  #                    setSeed = seed,
+  #                    constants = constants),
+  #   
+  #   both = nimbleMCMC(code = eq5dboth,
+  #                     data = bvnorm,
+  #                     inits = inits2, 
+  #                     nchains = n.chains,
+  #                     nburnin = burnin,
+  #                     niter = ifelse(MCMC*n.chains*thin == 0,
+  #                                    MCMC*n.chains + burnin,
+  #                                    MCMC*n.chains*thin + burnin),
+  #                     setSeed = seed,
+  #                     constants = constants))
+
+  nimblereg2 <- readRDS("nimble_test_with_sex.rds")
+  nimblereg2
+
 }
